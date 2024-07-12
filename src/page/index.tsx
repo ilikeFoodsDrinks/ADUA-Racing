9e7e50c37128881bce96bb92cc596f648c41e490
ilikeFoodsDrinks <louisle236@gmail.com>
2024-07-12 17:46:18 +0700

    first commit

---
+++ src/page/index.tsx
@@ -0,0 +1,219 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container w-[1512px] h-[3728px] bg-[#fff] relative overflow-hidden mx-auto my-0'>
+      <div className='w-[1512px] h-[418px] relative overflow-hidden z-[8] mt-[3082px] mr-0 mb-0 ml-0'>
+        <div className='w-[1413.3px] h-[232px] relative z-[22] mt-[70px] mr-0 mb-0 ml-[98.7px]'>
+          <div className='w-[605px] h-[232px] text-[0px] absolute top-0 left-[808.3px] z-[9]'>
+            <span className="block h-[116px] font-['Space_Grotesk'] text-[100px] font-bold leading-[116px] text-[#051924] tracking-[5px] relative text-left whitespace-nowrap z-[12] mt-0 mr-0 mb-0 ml-0">
+              CONTACT
+            </span>
+            <div className='w-[605px] h-[116px] relative z-[11] mt-0 mr-0 mb-0 ml-0'>
+              <button className='w-[605px] h-[116px] bg-[#a40000] border-none absolute top-0 left-0 z-10 pointer' />
+              <span className="flex h-[116px] justify-start items-center font-['Space_Grotesk'] text-[100px] font-bold leading-[116px] text-[#f5f5f5] tracking-[5px] absolute top-0 left-0 right-0 text-left whitespace-nowrap z-[11]">
+                US
+              </span>
+            </div>
+          </div>
+          <div className='w-[302.3px] h-[89px] absolute top-[24px] left-0 z-[22]'>
+            <span className="flex w-[202.617px] h-[80px] justify-start items-center font-['Space_Grotesk'] text-[25px] font-bold leading-[37.5px] text-[#000] tracking-[3.75px] absolute top-0 left-[99.684px] text-left z-[23]">
+              @aduaracing
+            </span>
+            <div className='w-[25.29%] h-[89.89%] bg-[url(../assets/images/f7da71f0-6643-4f06-99d2-6dca437e0d2f.png)] bg-[length:100%_100%] bg-no-repeat absolute top-[10.11%] left-0 z-[24]' />
+          </div>
+          <div className='w-[419.702px] h-[184px] absolute top-[24px] left-[337.3px] z-[16]'>
+            <span className="flex w-[306.702px] h-[184px] justify-start items-center font-['Space_Grotesk'] text-[25px] font-bold leading-[37.5px] text-[#000] tracking-[3.75px] absolute top-0 left-[113px] text-left z-[17]">
+              246 Nguyen Van Huong, Street, District 2,
+              <br />
+              Ho Chi Minh city
+            </span>
+            <div className='w-[19.06%] h-[43.48%] bg-[url(../assets/images/61a34410-e97a-4cac-9973-b193163445b0.png)] bg-[length:100%_100%] bg-no-repeat absolute top-[4.89%] left-0 z-[18]' />
+          </div>
+          <div className='w-[302.3px] h-[87px] absolute top-[140px] left-0 z-[19]'>
+            <span className="flex w-[202.617px] h-[80px] justify-start items-center font-['Space_Grotesk'] text-[25px] font-bold leading-[37.5px] text-[#000] tracking-[3.75px] absolute top-0 left-[99.684px] text-left z-20">
+              +81 17 9999 28
+            </span>
+            <div className='w-[25.29%] h-[91.95%] bg-[url(../assets/images/19c8ef78-f3fc-4990-961b-44323fc54e70.png)] bg-[length:100%_100%] bg-no-repeat absolute top-[8.05%] left-0 z-[21]' />
+          </div>
+        </div>
+        <div className='w-[417.05px] h-[84.222px] relative z-[15] mt-[22px] mr-0 mb-0 ml-[98.7px]'>
+          <div className='w-[19.18%] h-[94.99%] bg-[url(../assets/images/739b6157-2fbe-477e-a31f-eb4e21a8c56b.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[13]' />
+          <div className='w-[19.18%] h-[94.99%] bg-[url(../assets/images/02424aed-8853-48e1-a571-ec375c7d292a.png)] bg-[length:100%_100%] bg-no-repeat absolute top-[4.75%] left-[80.82%] z-[14]' />
+          <div className='w-[19.18%] h-[94.99%] bg-[url(../assets/images/d9f37afc-5ff7-465a-81c9-17f1f8e690f4.png)] bg-[length:100%_100%] bg-no-repeat absolute top-[5.01%] left-[40.28%] z-[15]' />
+        </div>
+      </div>
+      <div className='w-[1512px] h-[187px] relative overflow-hidden mt-[41px] mr-0 mb-0 ml-0'>
+        <div className='w-full h-full bg-[#d9d9d9] absolute top-0 left-0 z-[1]' />
+        <div className='w-[41.33%] h-[80.21%] absolute top-[11.76%] left-[52.31%] z-[4]'>
+          <div className='w-[150px] h-[150px] bg-[url(../assets/images/c87cbbcd7ae0111c709b59415f8836916033f8c2.png)] bg-cover bg-no-repeat absolute top-0 left-0 z-[5]' />
+          <div className='w-full h-[56.35%] absolute top-0 left-0 z-[6]' />
+          <span className="flex w-full h-[33.81%] justify-end items-start font-['Limelight'] text-[65px] font-normal leading-[50.714px] text-[#051924] tracking-[-0.65px] absolute top-[12.02%] left-0 text-right whitespace-nowrap z-[7]">
+            | ADUA Racing
+          </span>
+        </div>
+        <span className="flex w-[632.1px] h-[48%] justify-start items-start font-['Space_Grotesk'] text-[20px] font-light leading-[20px] text-[#000] tracking-[0.2px] absolute top-[21.39%] left-[6.55%] text-left z-[3]">
+          The F1 IN SCHOOLS Logo, F1, FORMULA 1, FIA FORMULA ONE WORLD
+          CHAMPIONSHIP, GRAND PRIX and related marks are trademarks of Formula
+          One Licensing BV, a Formula One group company. All rights reserved
+          <br />
+          <br />
+          Motto: Vroom
+        </span>
+        <span className="flex w-[18.47%] h-[16%] justify-end items-start font-['Space_Grotesk'] text-[40px] font-bold leading-[29.92px] text-[#000] tracking-[-0.4px] absolute top-[65.24%] left-[75.2%] text-right whitespace-nowrap z-[2]">
+          Vroooooooom
+        </span>
+      </div>
+      <div className='w-[1512px] h-[1300px] absolute top-0 left-0 overflow-hidden z-[71]'>
+        <div className='w-[1512px] h-[118px] relative z-[89] mt-0 mr-0 mb-0 ml-0'>
+          <div className='w-[1512px] h-[114px] bg-[#f5f5f5] rounded-tl-none rounded-tr-none rounded-br-[100px] rounded-bl-[100px] absolute top-0 left-0 z-[81]'>
+            <button className='w-[143.204px] h-[48px] bg-[#a40000] rounded-[8px] border-none absolute top-[37px] right-[562.873px] overflow-hidden z-[85] pointer'>
+              <span className="flex w-[72px] h-[32px] justify-center items-center font-['Inter'] text-[25px] font-semibold leading-[32px] text-[#f5f5f5] absolute top-[calc(50%-16.5px)] left-[calc(50%-35.6px)] text-center whitespace-nowrap z-[86]">
+                Home
+              </span>
+            </button>
+            <span className="flex w-[148.177px] h-[32px] justify-center items-center font-['Inter'] text-[25px] font-extralight leading-[32px] text-[#051924] absolute top-[45px] right-[100.442px] text-center whitespace-nowrap z-[84]">
+              Contact Us
+            </span>
+            <span className="flex w-[128.287px] h-[32px] justify-center items-center font-['Inter'] text-[25px] font-extralight leading-[32px] text-[#051924] absolute top-[45px] right-[408.729px] text-center whitespace-nowrap z-[82]">
+              Our Team
+            </span>
+            <span className="flex w-[121.326px] h-[32px] justify-center items-center font-['Inter'] text-[25px] font-extralight leading-[32px] text-[#051924] absolute top-[45px] right-[263.536px] text-center whitespace-nowrap z-[83]">
+              Sponsors
+            </span>
+          </div>
+          <div className='w-[597.083px] h-[114px] absolute top-[4px] left-[98.453px] z-[89]'>
+            <div className='w-[100px] h-[100px] bg-[url(../assets/images/c87cbbcd7ae0111c709b59415f8836916033f8c2.png)] bg-cover bg-no-repeat absolute top-0 left-0 z-[88]' />
+            <span className="flex w-[597.083px] h-[68.4px] justify-end items-start font-['Limelight'] text-[65px] font-normal leading-[65px] text-[#051924] tracking-[-0.65px] absolute top-[24.32px] left-0 text-right whitespace-nowrap z-[90]">
+              | ADUA Racing
+            </span>
+          </div>
+        </div>
+        <div className='w-[1921px] h-[1154.064px] relative z-[79] mt-[28px] mr-0 mb-0 ml-[-161px]'>
+          <div className='w-[669px] h-[483px] bg-[url(../assets/images/6fe4e6f4-bde6-486c-86cb-a816942cb3b1.png)] bg-cover bg-no-repeat absolute top-0 left-[277px] z-[78]' />
+          <span className="flex w-[559px] h-[288px] justify-start items-start font-['Space_Grotesk'] text-[80px] font-medium leading-[96px] text-[#051924] tracking-[-1.6px] absolute top-0 left-[1010px] right-[352px] text-left overflow-hidden z-[77]">
+            WE <br />
+            REPRESENT
+            <br />
+            VIETNAM
+          </span>
+          <div className='w-[1720.845px] h-[427.579px] bg-[url(../assets/images/71e30315-210d-4438-9da7-fb9855d16903.png)] bg-cover bg-no-repeat absolute top-[161px] left-[21px] z-[76]' />
+          <div className='w-[684px] h-[370px] absolute top-[465px] left-[161px] z-[72]'>
+            <button className='w-[450px] h-[72px] bg-cover bg-no-repeat border-none relative rotate-180 z-[73] pointer mt-0 mr-0 mb-0 ml-0' />
+            <span className="flex w-[587px] h-[266px] justify-start items-start font-['Space_Grotesk'] text-[25px] font-light leading-[37.5px] text-[#000] tracking-[2.5px] relative text-left z-[74] mt-[32px] mr-0 mb-0 ml-[97px]">
+              Formed in early 2023, we are comprised of five committed students
+              of the British International School Ho Chi Minh City to represent
+              Vietnam for F1-in-Schools. We invite you to join us on our journey
+              as we aim to take on the globe at the upcoming World Final.
+            </span>
+          </div>
+          <div className='w-[1194px] h-[508px] bg-[url(../assets/images/f9e89b84-0a07-4240-8c80-9f765c703c5b.png)] bg-cover bg-no-repeat absolute top-[483px] left-[727px] z-[79]' />
+          <div className='w-[1709.011px] h-[543.064px] bg-[url(../assets/images/5ad8a45c-8656-49fa-8eb4-901142ab1006.png)] bg-cover bg-no-repeat absolute top-[611px] left-0 z-[75]' />
+        </div>
+      </div>
+      <div className='w-[1512px] h-[1024px] absolute top-[1188px] left-0 overflow-hidden z-[41]'>
+        <div className='w-[1512px] h-[146px] relative z-[67] mt-[73px] mr-0 mb-0 ml-0'>
+          <button className='w-[502px] h-[146px] bg-[#a40000] border-none absolute top-0 left-[756px] z-[68] pointer' />
+          <span className="flex h-[145px] justify-center items-center font-['Space_Grotesk'] text-[100px] font-bold leading-[120px] text-[#f5f5f5] tracking-[-2px] absolute top-px left-0 right-0 text-center whitespace-nowrap z-[69]">
+            TAKING ON THE GLOBE
+          </span>
+          <span className="flex h-[120px] justify-center items-center font-['Space_Grotesk'] text-[100px] font-bold leading-[120px] text-[#051924] tracking-[-2px] absolute top-[13px] left-[223px] right-[735px] text-center whitespace-nowrap z-[70]">
+            TAKING ON
+          </span>
+        </div>
+        <div className='w-[1512px] h-[663px] relative z-[42] mt-[81px] mr-0 mb-0 ml-0'>
+          <button className='w-[309px] h-[72px] border-none relative z-[64] pointer mt-0 mr-0 mb-0 ml-0'>
+            <div className='w-[309px] h-[72px] bg-[#a40000] absolute top-0 left-0 z-[65]' />
+            <span className="flex h-[72px] justify-end items-start font-['Space_Grotesk'] text-[60px] font-medium leading-[72px] text-[#f5f5f5] tracking-[-1.2px] absolute top-0 left-0 right-0 text-right whitespace-nowrap z-[66]">
+              Our Team.
+            </span>
+          </button>
+          <div className='flex w-[1100px] gap-[50px] items-start flex-nowrap relative z-[43] mt-[59px] mr-0 mb-0 ml-[206px]'>
+            <div className='w-[180px] h-[353.6px] shrink-0 relative z-[44]'>
+              <div className='w-[180px] h-[220px] bg-[#bc5151] relative z-[47] mt-0 mr-0 mb-0 ml-0' />
+              <span className="block h-[45.6px] font-['Space_Grotesk'] text-[25px] font-bold leading-[37.5px] text-[#bc5151] tracking-[2.5px] relative text-left whitespace-nowrap z-[45] mt-[-2.8px] mr-0 mb-0 ml-0">
+                LOUIS LE,
+                <br />
+                16
+              </span>
+              <span className="block h-[45.6px] font-['Space_Grotesk'] text-[25px] font-light leading-[37.5px] text-[#bc5151] tracking-[2.5px] relative text-left whitespace-nowrap z-[46] mt-[45.2px] mr-0 mb-0 ml-0">
+                Digital
+              </span>
+            </div>
+            <div className='w-[180px] h-[353.6px] shrink-0 relative z-[48]'>
+              <div className='w-[180px] h-[220px] bg-[#bc5151] relative z-[51] mt-0 mr-0 mb-0 ml-0' />
+              <span className="block h-[45.6px] font-['Space_Grotesk'] text-[25px] font-bold leading-[37.5px] text-[#bc5151] tracking-[2.5px] relative text-left whitespace-nowrap z-50 mt-[-2.8px] mr-0 mb-0 ml-0">
+                KATHY DO,
+                <br />
+                16
+              </span>
+              <span className="block h-[45.6px] font-['Space_Grotesk'] text-[25px] font-light leading-[37.5px] text-[#bc5151] tracking-[2.5px] relative text-left whitespace-nowrap z-[49] mt-[45.2px] mr-0 mb-0 ml-0">
+                Marketing
+              </span>
+            </div>
+            <div className='w-[180px] h-[353.6px] shrink-0 relative z-[52]'>
+              <div className='w-[180px] h-[220px] bg-[#bc5151] relative z-[55] mt-0 mr-0 mb-0 ml-0' />
+              <span className="block h-[45.6px] font-['Space_Grotesk'] text-[25px] font-bold leading-[37.5px] text-[#bc5151] tracking-[2.5px] relative text-left whitespace-nowrap z-[54] mt-[-2.8px] mr-0 mb-0 ml-0">
+                MATHEO LUONG, 16
+              </span>
+              <span className="block h-[45.6px] font-['Space_Grotesk'] text-[25px] font-light leading-[37.5px] text-[#bc5151] tracking-[2.5px] relative text-left whitespace-nowrap z-[53] mt-[45.2px] mr-0 mb-0 ml-0">
+                Engineering
+              </span>
+            </div>
+            <div className='w-[180px] h-[353.6px] shrink-0 relative z-[56]'>
+              <div className='w-[180px] h-[220px] bg-[#bc5151] relative z-[59] mt-0 mr-0 mb-0 ml-0' />
+              <span className="block h-[45.6px] font-['Space_Grotesk'] text-[25px] font-bold leading-[37.5px] text-[#bc5151] tracking-[2.5px] relative text-left whitespace-nowrap z-[57] mt-[-2.8px] mr-0 mb-0 ml-0">
+                SOPHIE KAY,
+                <br />
+                16
+              </span>
+              <span className="block h-[45.6px] font-['Space_Grotesk'] text-[25px] font-light leading-[37.5px] text-[#bc5151] tracking-[2.5px] relative text-left whitespace-nowrap z-[58] mt-[45.2px] mr-0 mb-0 ml-0">
+                Social Media
+              </span>
+            </div>
+            <div className='w-[180px] h-[374px] shrink-0 relative z-[60]'>
+              <div className='w-[180px] h-[220px] bg-[#bc5151] relative z-[63] mt-0 mr-0 mb-0 ml-0' />
+              <span className="block h-[45.6px] font-['Space_Grotesk'] text-[25px] font-bold leading-[37.5px] text-[#bc5151] tracking-[2.5px] relative text-left whitespace-nowrap z-[62] mt-[-2.8px] mr-0 mb-0 ml-0">
+                SERENA LEE,
+                <br />
+                16
+              </span>
+              <span className="flex w-[180px] h-[66px] justify-start items-start font-['Space_Grotesk'] text-[25px] font-light leading-[37.5px] text-[#bc5151] tracking-[2.5px] relative text-left z-[61] mt-[45.2px] mr-0 mb-0 ml-0">
+                Team Principal
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='w-[1512px] h-[1024px] absolute top-[2017px] left-0 overflow-hidden z-[25]'>
+        <div className='w-[1733.146px] h-[411.174px] absolute top-[-14px] left-[-41px] z-[36]'>
+          <div className='w-[1733.146px] h-[411.174px] bg-[url(../assets/images/fc809a2b-bbb8-429b-ab6d-9f8e0b7b1832.png)] bg-cover bg-no-repeat absolute top-0 left-0 z-[37]' />
+          <div className='w-[549px] h-[146px] bg-[#a40000] absolute top-[146px] left-[761.845px] z-[38]' />
+          <span className="flex h-[145px] justify-center items-center font-['Space_Grotesk'] text-[100px] font-bold leading-[120px] text-[#f5f5f5] tracking-[-2px] absolute top-[147.307px] left-[147px] right-[74.146px] text-center whitespace-nowrap z-[39]">
+            PROUDLY SUPPORTED BY:
+          </span>
+          <span className="flex h-[120px] justify-center items-center font-['Space_Grotesk'] text-[100px] font-bold leading-[120px] text-[#051924] tracking-[-2px] absolute top-[160px] left-[242.845px] right-[936.301px] text-center whitespace-nowrap z-40">
+            PROUDLY
+          </span>
+        </div>
+        <div className='w-[519px] h-[661px] absolute top-[363px] left-[96px] z-[26]'>
+          <div className='w-[519px] h-[456px] bg-[url(../assets/images/6d9352c5d1662226832980c90f0639724cf94b09.png)] bg-cover bg-no-repeat rounded-[15px] relative z-[27] mt-0 mr-0 mb-0 ml-0' />
+          <div className='w-[188px] h-[202px] bg-[url(../assets/images/488eecc5f9f61413623e0ca9bed60115b840632f.png)] bg-cover bg-no-repeat rounded-[40px] absolute top-[459px] left-0 z-[28]' />
+          <div className='w-[320px] h-[192px] bg-[url(../assets/images/1b7b6d193653bd433ff693a78007e1b06dc25c26.png)] bg-cover bg-no-repeat rounded-[40px] absolute top-[467px] left-[199px] z-[29]' />
+        </div>
+        <div className='w-[670px] h-[656px] absolute top-[363px] left-[690px] z-30'>
+          <div className='w-[271.698px] h-[403px] absolute top-0 left-0 z-[35]'>
+            <div className='w-[216px] h-[216px] bg-[url(../assets/images/7af4f54fc8af2ed71927ab7e93d18da1cba6fca9.png)] bg-cover bg-no-repeat relative z-[31] mt-0 mr-0 mb-0 ml-[13px]' />
+            <div className='w-[271.698px] h-[120px] bg-[url(../assets/images/281c86dc85e4dde9b25c7db76c2f4d09750121ac.png)] bg-cover bg-no-repeat relative z-[35] mt-[67px] mr-0 mb-0 ml-0' />
+          </div>
+          <div className='w-[417px] h-[622px] absolute top-[34px] left-[253px] z-[34]'>
+            <div className='w-[377px] h-[99px] bg-[url(../assets/images/afff34a41d10952963b1c9ace04404ab633b1d10.png)] bg-cover bg-no-repeat relative z-[32] mt-0 mr-0 mb-0 ml-[40px]' />
+            <div className='w-[264.527px] h-[150px] bg-[url(../assets/images/ad2f9628b9ecd740589c409f73c202d0d7b95309.png)] bg-cover bg-no-repeat relative z-[33] mt-[83px] mr-0 mb-0 ml-[65px]' />
+            <div className='w-[260px] h-[233px] bg-[url(../assets/images/19d041eda3aed501270022731a563a4c018dd4a5.png)] bg-cover bg-no-repeat relative z-[34] mt-[57px] mr-0 mb-0 ml-0' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
